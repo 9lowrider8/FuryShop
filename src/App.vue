d240dcc2dbc8d6f6b30e8f3e42f02522807b9111
Mojtaba Parsaei <9lowrider8@gmail.com>
2022-09-02 11:00:00 +0430

    first slider

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,52 @@
 <template>
   <div>
-    <Header/>
-    <Footer/>
+    <Header />
+    <swiper :pagination="{
+      type: 'progressbar',
+    }" :navigation="true" :modules="modules" class="mySwiper">
+      <swiper-slide><img
+          src="https://dkstatics-public.digikala.com/digikala-adservice-banners/9e07d541b92f1564241b835c36174b35da6b31a5_1661089146.jpg?x-oss-process=image/quality,q_95"
+          alt="" srcset=""></swiper-slide>
+      <swiper-slide><img
+          src="https://dkstatics-public.digikala.com/digikala-adservice-banners/83324053d503e0389bc0078fbaa51f7fa860d613_1661340506.jpg?x-oss-process=image/quality,q_95"
+          alt="" srcset=""></swiper-slide>
+      <swiper-slide><img
+          src="https://dkstatics-public.digikala.com/digikala-adservice-banners/4d5f0655017c66ddd531aa9a13e60a755d5b81f9_1661780129.jpg?x-oss-process=image/quality,q_95"
+          alt="" srcset=""></swiper-slide>
+    </swiper>
+    <Footer />
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+
 export default {
   name: "App",
   components: {
     Header,
-    Footer
-  }
+    Footer,
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination, Navigation],
+    };
+  },
 }
 </script>
 
-<style>
+<style scoped>
+img {
+  width: 100%;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,63 @@
 <template>
   <header id="header">
     <div id="top">
-        <a href="/" id="logo"><img src="https://www.digikala.com/statics/img/svg/logo.svg" /></a>
-        <input type="search" placeholder="Search" class="form-input">
-        <a role="button" href="/" id="cart"><i class="fa fa-shopping-cart"></i><span>سبد خرید</span></a>
+      <a href="/" id="logo"><img src="https://www.digikala.com/statics/img/svg/logo.svg" /></a>
+      <input type="search" placeholder="جستجو با  نام کالا یا برند کالا" class="form-input">
+      <a role="button" href="/" id="cart"><i class="fa fa-shopping-cart"></i><span>سبد خرید</span></a>
     </div>
     <div id="bottom">
-        <div class="dropdown">
-        <div id="firstdl">
-            <button class="dropbtn"> صوتی و تصویری</button>
-                  <div class="dropdown-content">
-                    <a href="#"> موبایل</a>
-                    <a href="#"> لپ تاپ</a>
-                    <a href="#"> اسپیکر</a>
-                  </div>
-        </div>
-        <div id="seconddl">
-            <button class="dropbtn">لوازم خانگی</button>
-                  <div class="dropdown-content">
-                    <a href="#"> یخجال </a>
-                    <a href="#">  تلوزیون</a>
-                    <a href="#"> ماشین لباس شویی</a>
-                    <a href="#"> ماشین ظرفشویی</a>
-                    <a href="#"> ماکروویو</a>
-                  </div>
-
-        </div>
-        <div id="thirddl"><button class="dropbtn">لباس</button>
-                  <div class="dropdown-content">
-                    <a href="#"> کت</a>
-                    <a href="#"> شلوار</a>
-                    <a href="#"> کفش</a>
-                  </div>
-
-        </div>
-    </div>
+      <ul id="site-menu">
+        <li>
+          <i class="fa fa-shopping-cart"></i>
+          <span>متن مجموعه</span>
+          <ul>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+          </ul>
+        </li>
+        <li>
+          <i class="fa fa-shopping-cart"></i>
+          <span>متن مجموعه</span>
+          <ul>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+          </ul>
+        </li>
+        <li>
+          <i class="fa fa-shopping-cart"></i>
+          <span>متن مجموعه</span>
+          <ul>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+          </ul>
+        </li>
+        <li>
+          <i class="fa fa-shopping-cart"></i>
+          <span>متن مجموعه</span>
+          <ul>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+          </ul>
+        </li>
+        <li>
+          <i class="fa fa-shopping-cart"></i>
+          <span>متن مجموعه</span>
+          <ul>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+            <li><a href="#"><i class="fa fa-user"></i><span>متن زیرمجموعه</span></a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -46,5 +69,4 @@ export default {
 </script>
 
 <style>
-
 </style>
